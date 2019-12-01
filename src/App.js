236109ccf456ee5045e5c9df29877302f43b930f
Dalton <eschberger.dalton@gmail.com>
2019-11-30 21:11:45 -0600

    init

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  makeGrid(num){
+    const myNode = document.getElementById("grid");
+    while (myNode.firstChild) {
+    myNode.removeChild(myNode.firstChild);
+  }
+    for(var i=0;i<num;i++){
+      for(var j=1;j<=num;j++){
+        var node = document.createElement("div");                 // Create a <li> node
+        var textnode = document.createTextNode(num+" ");         // Create a text node
+        node.appendChild(textnode);                              // Append the text to <li>
+        document.getElementById("grid").appendChild(node); 
+      }
+    }
+  }
+  render(){
+    return (
+      <div className="App">
+        <div className="pageWrapper">
+          <div className="resetWrapper">
+            <button onClick={()=>this.makeGrid(16)} className="resetButton">Reset</button>
+          </div>
+          <div className="padWrapper">
+            <div className="sketchWrapper">
+              <div id="grid"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+  
 }
 
 export default App;
